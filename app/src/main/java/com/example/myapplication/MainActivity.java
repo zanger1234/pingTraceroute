d0bd1273c25fc272d8c12ed979447585d62076e0
Sief Addeen <saiffreitekh@gmail.com>
2022-05-11 15:16:33 +0300

    commit 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,35 @@
 package com.example.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        OkHttpClient okHttpClient = new OkHttpClient();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -45,45 +48,22 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-//            RequestBody formBody = new FormBody.Builder().add("sample", "text").build();
-//            Request request = new Request.Builder().url("http://127.0.0.1:5000/").post(formBody).build();
-//            okHttpClient.newCall(request).enqueue(new Callback() {
-//
-//                @Override
-//                public void onFailure(@NotNull Call call, @NotNull IOException e) {
-//                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), "server down", Toast.LENGTH_SHORT).show());
-//                }
-//                @Override
-//                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-//                    if (Objects.requireNonNull(response.body()).string().equals("received")) {
-//                        runOnUiThread(() -> Toast.makeText(getApplicationContext(), "data received", Toast.LENGTH_SHORT).show());
-//                    }
-//                }
-//            });
-            sendRequest("http://127.0.0.1:5000/");
-        });
-        findViewById(R.id.traceButton).setOnClickListener(v -> {
-            Intent intentTrace = new Intent(MainActivity.this, TraceActivity.class);
-            startActivity(intentTrace);
-            finish();
-        });
+            RequestBody formBody = new FormBody.Builder().add("sample", "text").build();
+            Request request = new Request.Builder().url("http://192.168.100.2:5000/").post(formBody).build();
+            okHttpClient.newCall(request).enqueue(new Callback() {
 
-    }
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void sendRequest(String backendURL){
-        try{
-            URL url = new URL(backendURL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            String outputJSON = "{\"test\":\"test\"}";
-            byte[] out = outputJSON.getBytes(StandardCharsets.UTF_8);
-            OutputStream stream = connection.getOutputStream();
-            ((OutputStream) stream).write(out);
-            connection.disconnect();
-        }catch (Exception e){
-            Toast.makeText(getApplicationContext(), "cannot reach server", Toast.LENGTH_SHORT).show();
-        }
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), "server down", Toast.LENGTH_SHORT).show());
+                }
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    if (Objects.requireNonNull(response.body()).string().equals("received")) {
+                        runOnUiThread(() -> Toast.makeText(getApplicationContext(), "data received", Toast.LENGTH_SHORT).show());
+                    }
+                }
+            });
+        });
     }
 
 
