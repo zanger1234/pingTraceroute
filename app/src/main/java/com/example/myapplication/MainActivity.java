5f4f6fce0edf603bde11d4a65c20463af1f86231
Sief Addeen <saiffreitekh@gmail.com>
2022-05-11 12:33:54 +0300

    commit 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,35 +4,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Objects;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
-    private OkHttpClient okHttpClient;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        okHttpClient = new OkHttpClient();
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = wifiManager.getConnectionInfo();
         String ssid = info.getSSID();
@@ -49,40 +45,48 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            RequestBody formbody
-                    = new FormBody.Builder()
-                    .add("sample", "text")
-                    .build();
-
-            // while building request
-            // we give our form
-            // as a parameter to post()
-            Request request = new Request.Builder().url("http://127.0.0.1:5000/debug").post(formbody).build();
-            okHttpClient.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), "server down", Toast.LENGTH_SHORT).show());
-                }
-
-                @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                    if (Objects.requireNonNull(response.body()).string().equals("received")) {
-                        runOnUiThread(() -> Toast.makeText(getApplicationContext(), "data received", Toast.LENGTH_SHORT).show());
-                    }
-                }
-            });
+//            RequestBody formBody = new FormBody.Builder().add("sample", "text").build();
+//            Request request = new Request.Builder().url("http://127.0.0.1:5000/").post(formBody).build();
+//            okHttpClient.newCall(request).enqueue(new Callback() {
+//
+//                @Override
+//                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+//                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), "server down", Toast.LENGTH_SHORT).show());
+//                }
+//                @Override
+//                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+//                    if (Objects.requireNonNull(response.body()).string().equals("received")) {
+//                        runOnUiThread(() -> Toast.makeText(getApplicationContext(), "data received", Toast.LENGTH_SHORT).show());
+//                    }
+//                }
+//            });
+            sendRequest("http://127.0.0.1:5000/");
         });
-
         findViewById(R.id.traceButton).setOnClickListener(v -> {
             Intent intentTrace = new Intent(MainActivity.this, TraceActivity.class);
             startActivity(intentTrace);
             finish();
         });
 
-
-
-
     }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void sendRequest(String backendURL){
+        try{
+            URL url = new URL(backendURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            String outputJSON = "{\"test\":\"test\"}";//
+            byte[] out = outputJSON.getBytes(StandardCharsets.UTF_8);
+            OutputStream stream = connection.getOutputStream();
+            ((OutputStream) stream).write(out);
+            System.out.println(connection.getResponseCode() + " " + connection.getResponseMessage()); // THis is optional
+            connection.disconnect();
+        }catch (Exception e){
+            System.out.println("Failed successfully");
+        }
+    }
+
 
     public String ping(String url) throws IOException {
         Process p = null;
@@ -101,15 +105,6 @@ public class MainActivity extends AppCompatActivity {
         StringBuilder res = new StringBuilder();
         while ((s = stdInput.readLine()) != null) {
             res.append(s).append("\n");
-//        String str = "/system/bin/ping -c 4 " + url;
-//        java.util.Scanner s = null;
-//        try {
-//            s = new java.util.Scanner(Runtime.getRuntime().exec(str).getInputStream()).useDelimiter("\\A");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        assert s != null;
-//        return s.hasNext() ? s.next() : "";
         }
         p.destroy();
         if (res.toString().equals("")) {
